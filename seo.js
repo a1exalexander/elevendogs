3c04b22fe91c297fb437605ecf2716746e0c665b
Alexander Ratushnyi <alexander.ratushnyi@gmail.com>
2020-12-30 17:00:24 +0200

    new seo

--- seo.js
+++ seo.js
@@ -3,7 +3,7 @@ module.exports = {
   type: 'website',
   title: 'Eleven Dogs | Kremenchuk Barbershop',
   img: '/elevendogs.png',
-  description: 'Барбершоп в центрі Кременчука',
+  description: 'Барбершоп в центрі Кременчука. Пострижись як в прошлий раз!',
   siteName: 'Barbershop',
   locale: 'uk_UA',
   author: 'Євгеній Кохно',
