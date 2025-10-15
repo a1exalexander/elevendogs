a138348d1f45170d08e48c6a4abf89e965ff1bc4
Oleksandr Ratushnyi <oleksandr.ratushnyi@namecheap.com>
2025-10-15 21:29:03 +0300

    Refactor code structure for improved readability and maintainability

---
+++ src/types/images.d.ts
@@ -0,0 +1,19 @@
+declare module '*.jpg' {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module '*.JPG' {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: StaticImageData;
+  export default content;
+}
+
+declare module '*.JPEG' {
+  const content: StaticImageData;
+  export default content;
+}
